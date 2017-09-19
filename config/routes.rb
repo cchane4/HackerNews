ceda5289527c733b183acb453826174faa525340
Chad Chaney <cchane4@gmail.com>
2017-09-19 17:23:46 -0500

    Add users with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :submissions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'submissions#index'
+#for root of application look in submissions folder, index file.
+# for root look in the index.html.erb file.
 end
