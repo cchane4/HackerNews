d068fc59a6008aa7316aedad50dc9e7cca69ecf8
Chad Chaney <cchane4@gmail.com>
2017-09-24 16:48:51 -0500

    added comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
   resources :submissions do
   	member do
   		put "like", to: "submission#upvote"
   		put "dislike", to: "submission#downvote"
   	end
+  	resources :comments
   end
 root 'submissions#index'
 #for root of application look in submissions folder, index file.
